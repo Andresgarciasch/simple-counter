bf42602114c0e907ee8c68a784035029e39a91ea
Andresgarciasch <andres_28garcia@outlook.es>
2022-05-05 08:16:41 +0000

    last

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,22 +1,43 @@
 import React from "react";
+import PropTypes from "prop-types";
 // import { fontawesome } from "@fortawesome/react-fontawesome";
 
 //create your first component
-const Home = () => {
+const Home = (props) => {
 	return (
 		<>
 			<div className="counter">
 				<div className="innercounter">
 					<div className="clock"></div>
-					<div className="five">0</div>
-					<div className="four">0</div>
-					<div className="three">0</div>
-					<div className="two">0</div>
-					<div className="one">0</div>
+					<div className="five"> {props.five} </div>
+					<div className="four"> {props.four} </div>
+					<div className="three">{props.three} </div>
+					<div className="two"> {props.two} </div>
+					<div className="one"> {props.one}</div>
 				</div>
 			</div>
 		</>
 	);
 };
+
+Home.propTypes = {
+	five: PropTypes.number,
+	four: PropTypes.number,
+	three: PropTypes.number,
+	two: PropTypes.number,
+	one: PropTypes.number,
+};
+
+let counter = 0;
+setInterval(() => {
+	const five = Math.floor(counter / 10000);
+	const four = Math.floor(counter / 1000);
+	const three = Math.floor(counter / 100);
+	const two = Math.floor(counter / 10);
+	const one = Math.floor(counter / 1);
+	console.log(five, four, three, two, one % 10);
+
+	counter++;
+}, 1000);
 
 export default Home;
--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,3 @@
-//import react into the bundle
 import React from "react";
 import ReactDOM from "react-dom";
 import "../styles/index.css";
@@ -6,5 +5,4 @@ import Home from "./component/home.jsx";
 
 //render your react application
 
-    ReactDOM.render(<Home />, document.querySelector("#app"));
-
+ReactDOM.render(<Home />, document.querySelector("#app"));
